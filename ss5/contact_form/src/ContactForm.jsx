fa19fa4cd1fada51ff7e47977dfb19f66f425cbd
LamTamNhu <46390185+LamTamNhu@users.noreply.github.com>
2024-01-29 15:13:59 +0700

    +contact_form

---
+++ ss5/contact_form/src/ContactForm.jsx
@@ -0,0 +1,52 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+
+const ContactSchema = Yup.object().shape({
+  name: Yup.string(),
+  email: Yup.string().email('Not proper email').required("Required"),
+  phone: Yup.string(),
+  address: Yup.string()
+});
+
+export function ContactForm() {
+  return (
+    <>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          phone: "",
+          message: "",
+        }}
+        validationSchema={ContactSchema}  
+        onSubmit={values => {
+          alert("Submitted");
+          console.log(values);
+        }}      
+      >
+        <Form className="container mt-3">
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <Field className="form-control" type="text" id="name" name="name"></Field>
+          <label className="form-label" htmlFor="email">
+            Email
+          </label>
+          <Field className="form-control" type="text" id="email" name="email"></Field>
+          <ErrorMessage name="email" className="text-danger" component='p'/>
+          <label className="form-label" htmlFor="phone">
+            Phone
+          </label>
+          <Field className="form-control" type="text" id="phone" name="phone"></Field>
+          <label className="form-label" htmlFor="message">
+            Message
+          </label>
+          <Field className="form-control" type="text" id="message" name="message"></Field>
+          <button className="btn btn-primary mt-3" type="submit">
+            Submit
+          </button>
+        </Form>
+      </Formik>
+    </>
+  );
+}
